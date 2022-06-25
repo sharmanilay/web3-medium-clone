cb351a05fc78909b3aa65062ff39794c28c7210f
Nilay Sharma <nilaysharma642@gmail.com>
2022-06-25 23:43:36 +0530

    update contracts

--- smartcontract/Medium.sol
+++ smartcontract/Medium.sol
@@ -10,4 +10,48 @@ contract Medium is ERC721, ERC721URIStorage, Ownable {
 
     Counters.counter private _tokenIdCounter;
     uint256 public fees;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 fees_
+    ) ERC721(name_, symbol_) {
+        fees = fees_;
+    }
+
+    function safeMint(address to, string memory uri) public payable {
+        require(msg.value >= fees, "Not Enough Matic");
+        payable(owner().transfer(fees));
+
+        // Mint NFT
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+
+        // Return oversupplied fees
+        uint256 contractBalance = address(this).balance;
+
+        if (contractBalance > 0) {
+            payable(msg.sender).transfer(address(this).balance);
+        }
+    }
+
+    // Override functions
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
